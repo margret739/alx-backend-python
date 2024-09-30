79c4e362acd73964b6bf1189d970e89789638d1e
margret739 <margretandalya@gmail.com>
2024-09-30 11:33:50 +0000

    async task0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> Generator[float, None, None]:
     '''Generates a sequence of 10 numbers.'''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() *10
+        yield random.random() * 10
